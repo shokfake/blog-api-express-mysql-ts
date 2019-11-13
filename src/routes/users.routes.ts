3bf6e83d5990eff757e1e52f316768b8757dbf47
Jorge Hernandez <jorge@cbqasolutions.com>
2019-11-13 12:43:30 -0600

    Add API docs

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -16,7 +16,53 @@ import {
 
 const router = Router();
 
-// todo: add api docs
+/**
+ * @api {POST} /api/v1/users
+ * @apiName Create a new user
+ * @apiGroup Users
+ *
+ * @apiDescription Adds a new user to the database. Returns the new user in JSON format.
+ *
+ * @apiParam  username    {string}  Username of the user, should be unique.
+ * @apiParam  displayName {string}  Name of the user to show in the application.
+ * @apiParam  bio         {string}  (Optional) Short biography of the user.
+ * @apiParam  birthDate   {string}  Birth date of the user, should be in yyyy-mm-dd format.
+ *
+ * @apiParamExample {json} Example request
+ *  HTTP/1.1
+ *  {
+ *    "username": "jorgehdzg1991",
+ *    "displayName": "Jorge Hernandez",
+ *    "bio": "Lorem ipsum dolor sit amet.",
+ *    "birthDate": "1991-12-16"
+ *  }
+ *
+ * @apiSuccess  id          {number}  ID of the user.
+ * @apiSuccess  username    {string}  Username of the user.
+ * @apiSuccess  displayName {string}  Name of the user to show in the application..
+ * @apiSuccess  bio         {string}  Short biography of the user.
+ * @apiSuccess  birthDate   {string}  Birth date of the user, should be in yyyy-mm-dd format.
+ * @apiSuccess  createDate  {string}  Creation date of the user, should be in yyyy-mm-dd hh:ii:ss format.
+ * @apiSuccess  lastUpdated {string}  Date on which the user was last updated, should be in yyyy-mm-dd hh:ii:ss format.
+ * @apiSuccess  status      {boolean} Indicator if the user is active or not.
+ *
+ * @apiSuccessExample {json} Success response
+ *  HTTP/1.1 200 OK
+ *  {
+ *    "username": "jorgehdzg1991",
+ *    "displayName": "Jorge Hernandez",
+ *    "bio": "Lorem ipsum dolor sit amet.",
+ *    "birthDate": "1991-12-16",
+ *    "id": 1,
+ *    "createDate": "2019-11-13T18:23:13.000Z",
+ *    "lastUpdated": "2019-11-13T18:23:13.000Z",
+ *    "status": true
+ *  }
+ *
+ * @apiError  (4xx) BadRequest          The request body is malformed
+ * @apiError  (4xx) Conflict            The specified username is already taken.
+ * @apiError  (5xx) InternalServerError Unknown error occurred.
+ */
 router.post(
   '/',
   getPostUserValidators(),
