0d3a9b612b66abb1fa53a81010c7c6a393be9ec4
Jorge Hernandez <jorge@cbqasolutions.com>
2019-11-15 22:56:45 -0600

    add test directory to lint scope & suppress console messages during tests

--- test/routes/user.routes.spec.ts
+++ test/routes/user.routes.spec.ts
@@ -1,7 +1,7 @@
 import expect from 'expect';
 import sinon, { SinonStubbedInstance, SinonStub } from 'sinon';
 import request, { Response } from 'supertest';
-import { Connection, EntityManager, SaveOptions } from 'typeorm';
+import { Connection, SaveOptions } from 'typeorm';
 import {
   BAD_REQUEST,
   CONFLICT,
@@ -22,158 +22,164 @@ const userData = {
 };
 
 describe('user routes tests', () => {
-  let getConnectionStub: SinonStub<[], Promise<Connection>>;
-  let connectionStub: SinonStubbedInstance<Connection>;
-  let userSaveStub: SinonStub<[(SaveOptions | undefined)?], Promise<User>>;
+  describe('POST /api/v1/users', () => {
+    let getConnectionStub: SinonStub<[], Promise<Connection>>;
+    let connectionStub: SinonStubbedInstance<Connection>;
+    let userSaveStub: SinonStub<[(SaveOptions | undefined)?], Promise<User>>;
 
-  beforeEach(() => {
-    connectionStub = sinon.createStubInstance(Connection);
+    beforeEach(() => {
+      connectionStub = sinon.createStubInstance(Connection);
 
-    getConnectionStub = sinon.stub(utils, 'getConnection');
-    getConnectionStub.returns(Promise.resolve(connectionStub as any));
+      getConnectionStub = sinon.stub(utils, 'getConnection');
+      getConnectionStub.returns(Promise.resolve(connectionStub as any));
 
-    userSaveStub = sinon.stub(User.prototype, 'save');
-  });
+      userSaveStub = sinon.stub(User.prototype, 'save');
+    });
 
-  afterEach(() => {
-    getConnectionStub.restore();
-    userSaveStub.restore();
-  });
+    afterEach(() => {
+      getConnectionStub.restore();
+      userSaveStub.restore();
+    });
 
-  it('should be able to POST a user', done => {
-    const fakeUser = new User(
-      userData.username,
-      userData.displayName,
-      userData.bio,
-      userData.birthDate
-    );
-    fakeUser.id = fakeId;
-    userSaveStub.returns(Promise.resolve(fakeUser));
-    request(app)
-      .post('/api/v1/users')
-      .send(userData)
-      .expect('Content-Type', /json/)
-      .expect(OK)
-      .end((err, res: Response) => {
-        if (err) {
-          done(err);
-        } else {
-          const { body } = res;
-          expect(body).toEqual({
-            ...userData,
-            id: fakeId
-          });
-          expect(getConnectionStub.called).toBeTruthy();
-          expect(getConnectionStub.callCount).toBe(1);
-          expect(userSaveStub.called).toBeTruthy();
-          expect(userSaveStub.callCount).toBe(1);
-          done();
-        }
-      });
-  });
+    it('should be able to POST a user', done => {
+      const fakeUser = new User(
+        userData.username,
+        userData.displayName,
+        userData.bio,
+        userData.birthDate
+      );
+      fakeUser.id = fakeId;
+      userSaveStub.returns(Promise.resolve(fakeUser));
+      request(app)
+        .post('/api/v1/users')
+        .send(userData)
+        .expect('Content-Type', /json/)
+        .expect(OK)
+        .end((err, res: Response) => {
+          if (err) {
+            done(err);
+          } else {
+            const { body } = res;
+            expect(body).toEqual({
+              ...userData,
+              id: fakeId
+            });
+            expect(getConnectionStub.called).toBeTruthy();
+            expect(getConnectionStub.callCount).toBe(1);
+            expect(userSaveStub.called).toBeTruthy();
+            expect(userSaveStub.callCount).toBe(1);
+            done();
+          }
+        });
+    });
 
-  it('should respond 409 Conflict if "Username is already taken."', done => {
-    const error = { code: 'ER_DUP_ENTRY' };
-    userSaveStub.throws(error);
+    it('should respond 409 Conflict if "Username is already taken."', done => {
+      const error = { code: 'ER_DUP_ENTRY' };
+      userSaveStub.throws(error);
 
-    request(app)
-      .post('/api/v1/users')
-      .send(userData)
-      .expect('Content-Type', /json/)
-      .expect(CONFLICT)
-      .end((err, res: Response) => {
-        if (err) {
-          done(err);
-        } else {
-          const { body } = res;
-          expect(body).toEqual({ message: 'Username is already taken.' });
-          done();
-        }
-      });
-  });
+      request(app)
+        .post('/api/v1/users')
+        .send(userData)
+        .expect('Content-Type', /json/)
+        .expect(CONFLICT)
+        .end((err, res: Response) => {
+          if (err) {
+            done(err);
+          } else {
+            const { body } = res;
+            expect(body).toEqual({ message: 'Username is already taken.' });
+            done();
+          }
+        });
+    });
 
-  it('should respond 500 Internal Server Error if unknown error is thrown', done => {
-    const error = { code: 'FAKE-CODE' };
-    userSaveStub.throws(error);
+    it('should respond 500 Internal Server Error if unknown error is thrown', done => {
+      const error = { code: 'FAKE-CODE' };
+      userSaveStub.throws(error);
 
-    request(app)
-      .post('/api/v1/users')
-      .send(userData)
-      .expect('Content-Type', /json/)
-      .expect(INTERNAL_SERVER_ERROR)
-      .end((err, res: Response) => {
-        if (err) {
-          done(err);
-        } else {
-          const { body } = res;
-          expect(body).toEqual({ message: 'Unknown error.' });
-          done();
-        }
-      });
-  });
+      request(app)
+        .post('/api/v1/users')
+        .send(userData)
+        .expect('Content-Type', /json/)
+        .expect(INTERNAL_SERVER_ERROR)
+        .end((err, res: Response) => {
+          if (err) {
+            done(err);
+          } else {
+            const { body } = res;
+            expect(body).toEqual({ message: 'Unknown error.' });
+            done();
+          }
+        });
+    });
 
-  it("shouldn't call close in connection if failed to establish connection", done => {
-    getConnectionStub.throws({ fake: 'error' });
-    request(app)
-      .post('/api/v1/users')
-      .send(userData)
-      .expect('Content-Type', /json/)
-      .expect(INTERNAL_SERVER_ERROR)
-      .end((err, res: Response) => {
-        if (err) {
-          done(err);
-        } else {
-          const { body } = res;
-          expect(body).toEqual({ message: 'Unknown error.' });
-          done();
-        }
-      });
-  });
+    it("shouldn't call close in connection if failed to establish connection", done => {
+      getConnectionStub.throws({ fake: 'error' });
+      request(app)
+        .post('/api/v1/users')
+        .send(userData)
+        .expect('Content-Type', /json/)
+        .expect(INTERNAL_SERVER_ERROR)
+        .end((err, res: Response) => {
+          if (err) {
+            done(err);
+          } else {
+            const { body } = res;
+            expect(body).toEqual({ message: 'Unknown error.' });
+            done();
+          }
+        });
+    });
 
-  it('should return bad request if body is incomplete', done => {
-    getConnectionStub.throws({ fake: 'error' });
-    request(app)
-      .post('/api/v1/users')
-      .send({})
-      .expect('Content-Type', /json/)
-      .expect(BAD_REQUEST)
-      .end((err, res: Response) => {
-        if (err) {
-          done(err);
-        } else {
-          const { body } = res;
-          expect(body.messages).toBeDefined();
-          expect(body.messages).toContain('Parameter "username" is required.');
-          expect(body.messages).toContain(
-            'Parameter "username" length must be between 1 and 16 characters.'
-          );
-          expect(body.messages).toContain(
-            'Parameter "displayName" is required.'
-          );
-          expect(body.messages).toContain(
-            'Parameter "displayName" length must be between 1 and 16 characters.'
-          );
-          expect(body.messages).toContain('Parameter "birthDate" is required.');
-          done();
-        }
-      });
-  });
+    it('should return bad request if body is incomplete', done => {
+      getConnectionStub.throws({ fake: 'error' });
+      request(app)
+        .post('/api/v1/users')
+        .send({})
+        .expect('Content-Type', /json/)
+        .expect(BAD_REQUEST)
+        .end((err, res: Response) => {
+          if (err) {
+            done(err);
+          } else {
+            const { body } = res;
+            expect(body.messages).toBeDefined();
+            expect(body.messages).toContain(
+              'Parameter "username" is required.'
+            );
+            expect(body.messages).toContain(
+              'Parameter "username" length must be between 1 and 16 characters.'
+            );
+            expect(body.messages).toContain(
+              'Parameter "displayName" is required.'
+            );
+            expect(body.messages).toContain(
+              'Parameter "displayName" length must be between 1 and 16 characters.'
+            );
+            expect(body.messages).toContain(
+              'Parameter "birthDate" is required.'
+            );
+            done();
+          }
+        });
+    });
 
-  it('should return bad request if birthDate is greater than today', done => {
-    getConnectionStub.throws({ fake: 'error' });
-    request(app)
-      .post('/api/v1/users')
-      .send({ ...userData, birthDate: new Date().addDays(1) })
-      .expect('Content-Type', /json/)
-      .expect(BAD_REQUEST)
-      .end((err, res: Response) => {
-        if (err) {
-          done(err);
-        } else {
-          const { body } = res;
-          expect(body.messages).toBeDefined();
-          done();
-        }
-      });
+    it('should return bad request if birthDate is greater than today', done => {
+      getConnectionStub.throws({ fake: 'error' });
+      request(app)
+        .post('/api/v1/users')
+        .send({ ...userData, birthDate: new Date().addDays(1) })
+        .expect('Content-Type', /json/)
+        .expect(BAD_REQUEST)
+        .end((err, res: Response) => {
+          if (err) {
+            done(err);
+          } else {
+            const { body } = res;
+            expect(body.messages).toBeDefined();
+            done();
+          }
+        });
+    });
   });
 });
